be89648564a6cae0118416943767bd215cfa75cc
Jonathan Fenoud-Viard <jonathan.fenoud-viard@outlook.fr>
2023-06-06 10:05:45 +0200

    add Navbar component

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+const NavBar = () =>{
+    return(
+        <div className="nav-bar">
+            <nav className="bg-gray-800">
+                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                    <div className="relative flex h-16 items-center justify-between">
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            {/* <!-- Mobile menu button--> */}
+                            <button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                                <span className="sr-only">Open main menu</span>
+                                {/* <!--
+                                    Icon when menu is closed.
+
+                                    Menu open: "hidden", Menu closed: "block"
+                                --> */}
+                                <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                </svg>
+                                {/* <!--
+                                    Icon when menu is open.
+
+                                    Menu open: "block", Menu closed: "hidden"
+                                --> */}
+                                <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <div className="flex items-center">
+                                <p className="text-white ml-2 text-4xl font-bold text-transparent bg-gradient-to-r from-blue-500 via-white to-red-500 bg-clip-text tracking-wider">UDSP14</p>
+                            </div>
+                        </div>
+                        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            <button type="button" className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                <span className="sr-only">View notifications</span>
+                                <Link to="/login" className="block rounded py-2 pr-4 pl-3 text-white" aria-current="page">Connexion</Link>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="sm:hidden" id="mobile-menu">
+                    <div className="space-y-1 px-2 pb-3 pt-2">
+                        {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                        <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
+                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+                        </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
