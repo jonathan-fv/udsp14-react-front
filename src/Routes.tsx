8273c2d3865ff1fe52bf431d457935da21bebae3
Paul-blgrn <paul.blgrn@gmail.com>
2023-06-06 15:01:39 +0200

    add audio page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,6 @@
 import {Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import SituationPage from "./pages/SituationPage";
+import AudioPage from "./pages/AudioPage";
 import Home from "./pages/Home";
 
 const MainRoute = () => {
@@ -8,6 +8,7 @@ const MainRoute = () => {
       <Routes>
           <Route path='/login' element={<LoginPage/>}/>
           <Route path='/situation' element={<SituationPage />}/>
+          <Route path='/audio' element={<AudioPage />}/>
           <Route path="/" element={<Home/>} />
       </Routes>
     );
---
+++ src/pages/AudioPage.tsx
@@ -0,0 +1,7 @@
+const AudioPage = () => {
+    return (
+        <h1>Audio Page</h1>
+    );
+}
+
+export default AudioPage;
