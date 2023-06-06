c6f533acaef4ae6b32c9a2316e74b4ec8c25fa35
Killian <killian.wauterspro@gmail.com>
2023-06-06 16:21:15 +0200

    [correction-commit-bug]

--- src/pages/AudioPage.tsx
+++ src/pages/AudioPage.tsx
@@ -3,40 +3,43 @@ import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 const AudioPage = () => {
-    const navigate = useNavigate();
-    const handleGoBack = () => {
-        navigate(-1);
-    }
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1);
+  };
 
-    return (
-        <div>
-            <div className="flex justify-center">
-                <button onClick={handleGoBack} className="return-button-audio w-10 bg-gray-200 rounded-full m-4 p-1 text-2xl">
-                &#706;
-                </button>
-                <h1 className="p-5">Audio Page</h1>
-            </div>
-            <div className="img-box-audio bg-gray-200 rounded-xl"></div>
-            <div className="text-box-audio p-1 text-justify">
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
-                optio exercitationem autem, ut ducimus aspernatur fugiat sapiente
-                provident eius molestiae praesentium. Molestias atque, cum impedit
-                reiciendis et ullam distinctio provident error perspiciatis est alias
-                culpa perferendis, obcaecati repellendus! Provident doloremque ea
-                delectus perspiciatis, quasi fugiat odio sapiente rem rerum officia!
-                </p>
-            </div>
-            <div className="cta-box flex justify-center mt-10">
-                <Link
-                to="/"
-                className="cta-btn-audio rounded-full bg-blue-500 hover:bg-blue-600"
-                >
-                🏠
-                </Link>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <div className="flex justify-center">
+        <button
+          onClick={handleGoBack}
+          className="return-button w-10 bg-gray-200 rounded-full m-4 p-1 text-2xl"
+        >
+          &#706;
+        </button>
+        <h1 className="p-5">Audio Page</h1>
+      </div>
+      <div className="img-box-audio bg-gray-200 rounded-xl"></div>
+      <div className="text-box-audio p-1 text-justify">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+          optio exercitationem autem, ut ducimus aspernatur fugiat sapiente
+          provident eius molestiae praesentium. Molestias atque, cum impedit
+          reiciendis et ullam distinctio provident error perspiciatis est alias
+          culpa perferendis, obcaecati repellendus! Provident doloremque ea
+          delectus perspiciatis, quasi fugiat odio sapiente rem rerum officia!
+        </p>
+      </div>
+      <div className="cta-box flex justify-center mt-10">
+        <Link
+          to="/"
+          className="cta-btn-audio rounded-full bg-blue-500 hover:bg-blue-600"
+        >
+          🏠
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default AudioPage;
