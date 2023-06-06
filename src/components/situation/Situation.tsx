55cdc915e4cc3c74516de7bfdb7be777028b6d9e
Killian <killian.wauterspro@gmail.com>
2023-06-06 14:49:11 +0200

    [update-page-situation]

--- src/components/situation/Situation.tsx
+++ src/components/situation/Situation.tsx
@@ -1,22 +1,24 @@
-import { Link } from "react-router-dom"
-import './Situation.css'
+import { Link } from "react-router-dom";
+import "./Situation.css";
 
 type Props = {
-    situation: string;
-}
+  situation: string;
+};
 
-const Situation = (props : Props) => {
-    const { situation } = props;
-    return (
-        <Link to="" className="bg-gray-100 shadow-lg shadow-black-500/50 rounded-md tracking-widest uppercase font-bold w-96 p-5 m-10 text-center link-situation hover:bg-gray-300">
-            { situation }
-        </Link>
-        
-    )
-}
+const Situation = (props: Props) => {
+  const { situation } = props;
+  return (
+    <Link
+      to="/situation"
+      className="bg-gray-100 shadow-lg shadow-black-500/50 rounded-md tracking-widest uppercase font-bold w-96 p-5 m-10 text-center link-situation hover:bg-gray-300"
+    >
+      {situation}
+    </Link>
+  );
+};
 
 Situation.defaultProps = {
-    situation: 'Situation par defaut',
+  situation: "Situation par defaut",
 } as Props;
 
-export default Situation
+export default Situation;
--- src/pages/SituationPage.tsx
+++ src/pages/SituationPage.tsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
+import "../App.css";
+
 const SituationPage = () => {
-    return (
-        <h1>Situation page</h1>
-    );
-}
+  return (
+    <div>
+      <div className="flex justify-center">
+        <button className="return-button w-10 bg-gray-200 rounded-full m-4 p-1 text-2xl">
+          &#706;
+        </button>
+        <h1 className="p-5">Situation page</h1>
+      </div>
+      <div className="img-box bg-gray-200 rounded-xl"></div>
+      <div className="text-box p-1 text-justify">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+          optio exercitationem autem, ut ducimus aspernatur fugiat sapiente
+          provident eius molestiae praesentium. Molestias atque, cum impedit
+          reiciendis et ullam distinctio provident error perspiciatis est alias
+          culpa perferendis, obcaecati repellendus! Provident doloremque ea
+          delectus perspiciatis, quasi fugiat odio sapiente rem rerum officia!
+        </p>
+      </div>
+      <div className="cta-box flex justify-center mt-10">
+        <Link
+          to="/audio"
+          className="cta-btn rounded-full bg-blue-500 hover:bg-blue-600"
+        >
+          112
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default SituationPage;
