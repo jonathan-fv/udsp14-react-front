537f037f17ac7b1b83336ba6645e2386ff616d46
Prisca C <prisca.castanier@gmail.com>
2023-06-07 09:28:48 +0200

    fix (routes): fix routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,15 +6,19 @@ import Home from "./pages/Home";
 import CreateSituationPage from "./pages/CreateSituationPage";
 
 const MainRoute = () => {
-    return (
-      <Routes>
-          <Route path='/situation' element={<SituationPage />}/>
-          <Route path='/audio' element={<AudioPage />}/>
-          <Route path="/" element={<Home/>} />
-          <Route path='/login' element={<LoginPage />}/>
-          <Route path='/test' element={<CreateSituationPage />}/>
-      </Routes>
-    );
+  return (
+    <Routes>
+      <Route element={<FrontLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path='/situation' element={<SituationPage />}/>
+        <Route path='/audio' element={<AudioPage />}/>
+      </Route>
+      <Route path={'/administration'} element={<BackLayout />}>
+        <Route path='create-situation' element={<CreateSituationPage />}/>
+      </Route>
+    </Routes>
+  );
 }
 
 export default MainRoute;
