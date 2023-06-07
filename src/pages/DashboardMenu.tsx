c53705d03fdb27e3b8b6927a1e6d9d9951e4444e
Kiichi14 <100412917+Kiichi14@users.noreply.github.com>
2023-06-07 15:54:35 +0200

    add to dashboard situation page

--- src/pages/DashboardMenu.tsx
+++ src/pages/DashboardMenu.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const DashboardMenu = () => {
     return (
         <>
@@ -7,12 +9,12 @@ const DashboardMenu = () => {
             </div>
             <div className="menu-button">
                 <div className="menu-button">
-                    <a href="#" className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white active:bg-pink-600 font-bold uppercase px-8 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Liste de toutes les situations</a>
+                    <a href="situations" className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white active:bg-pink-600 font-bold uppercase px-8 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Liste de toutes les situations</a>
                 </div>
             </div>
             <div className="menu-button">
                 <div className="menu-button">
-                    <a href="#" className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white active:bg-pink-600 font-bold uppercase px-8 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Utilisateur</a>
+                    <a href="/administration" className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white active:bg-pink-600 font-bold uppercase px-8 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Utilisateur</a>
                 </div>
             </div>
         </div>
---
+++ src/pages/DashboardSituations.tsx
@@ -0,0 +1,50 @@
+import SituationsPanel from '../components/SituationPanel/SituationPanel'
+import axios from "axios";
+import { useState, useEffect } from 'react';
+
+const DashboardSituations = () => {
+
+    const [situations, setSituations] = useState(null);
+
+    const baseURL = "http://localhost:8000/situations";
+
+    useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setSituations(response.data);
+        });
+      }, []);
+
+    const deleteSituation = (id: string) => {
+        axios.delete(`${baseURL}/${id}`).then(() => {
+            alert("Situations Supprimer!");
+            window.location.reload();
+      });
+    };
+
+    const renderListSituations = (situations: any[]) => {
+        return situations.map(situation => 
+            <SituationsPanel
+                key={situation._id}
+                title={situation.situation.title}
+                desc={situation.situation.description}
+                delete={() => deleteSituation(situation._id)}
+            />
+        )
+    }
+
+    if (!situations) return (<p>Aucune Situation présente !</p>)
+
+    return (
+        <>
+            <div className="situations-title">
+                <h1>Toutes les Situations</h1>
+            </div>
+            <div className="situations-container">
+            {renderListSituations(situations)}
+            </div>
+        </>
+    );
+
+};
+
+export default DashboardSituations;
