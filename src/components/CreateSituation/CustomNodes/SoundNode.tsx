5c380fd853e01584a22fdb7cbf590d6d6eb3489d
Prisca C <prisca.castanier@gmail.com>
2023-06-07 13:57:41 +0200

    fix: fix issue after rebase

--- src/components/CreateSituation/CustomNodes/SoundNode.tsx
+++ src/components/CreateSituation/CustomNodes/SoundNode.tsx
@@ -1,6 +1,6 @@
 import {memo, useCallback, useState} from "react";
 import { Handle, Position } from "reactflow";
-import RemoveNodeButton from "./RemoveNodeButton";
+import SelectedNode from "./SelectedNode";
 
 const SoundImage = ({data, isConnectable, selected}: any) => {
     const [image, setImage] = useState('Image');
@@ -9,11 +9,13 @@ const SoundImage = ({data, isConnectable, selected}: any) => {
         data.label = evt.target.value;
     }, []);
 
+    const outline = selected ? '2px solid blue' : '0px';
+
     return (
-        <div style={{border: '1px solid red', padding: '10px', width: '100px', position: 'relative', overflow: 'hidden'}}>
-            <RemoveNodeButton selected={selected} removeNode={() => console.log('remove node')} />
+        <div style={{border: '1px solid purple', padding: '5px', width: '400px', position: 'relative', outline: outline }}>
+            <SelectedNode selected={selected} />
             <input type={"file"} accept={"audio/*"} onChange={onChange}></input>
-            <Handle type="target" position={Position.Right} style={{ borderRadius: 0 }} isConnectable id="b" className="!bg-teal-500 !h-[10px]"/>
+            <Handle type="target" position={Position.Right} style={{ borderRadius: 0 }} isConnectable={isConnectable} id="b" className="!bg-teal-500 !h-[10px]"/>
         </div>
     );
 };
