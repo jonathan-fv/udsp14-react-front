0a0719d6aec0f14bd934b67bc123b8dca8c5bcbd
MaelBriantin <mael.briantin@gmail.com>
2023-06-08 17:46:58 +0200

    bugfix(reactflow): bug correction for media nodes

--- src/components/CreateSituation/CustomNodes/ImageNode.tsx
+++ src/components/CreateSituation/CustomNodes/ImageNode.tsx
@@ -1,10 +1,13 @@
-import {memo, useCallback, useState} from "react";
+import {ChangeEvent, memo, useCallback, useEffect, useState} from "react";
 import { Handle, Position } from "reactflow";
 import SelectedNode from "./SelectedNode";
+
 import imageLogo from "../../../assets/images/image.png";
 
 const ImageNode = ({data, isConnectable, selected}: any) => {
-    const [image, setImage] = useState('Aucune image sélectionnée');
+    const inputId = `input-${Date.now()}`;
+    const [image, setImage] = useState(data.label);
+
     const onChange = useCallback((evt: any) => {
         setImage(evt.target.files[0].name);
         data.label = evt.target.files[0].name;
@@ -15,22 +18,14 @@ const ImageNode = ({data, isConnectable, selected}: any) => {
     return (
         <div  style={{border: '2px solid purple', padding: '5px', position: 'relative', outline: outline }} className={"w-12 h-12 flex items-center justify-center rounded-full"}>
             <SelectedNode selected={selected} />
-            <label htmlFor="image-upload"
+            <label htmlFor={inputId}
                        className="inline-block w-5 h-5 rounded-full bg-white cursor-pointer">
                     <img src={imageLogo} alt="une icone symbolisant une image" />
                 </label>
-            <input type={"file"} id="image-upload" className={"hidden"} accept={"image/*"} onChange={onChange}></input>
+            {/*<input type={"file"} accept={"image/*"} onChange={onChange}></input>*/}
+            <input type={"file"} id={inputId} className={"hidden"} accept={"image/*"} onChange={onChange}></input>
             <Handle type="target" position={Position.Right} style={{ borderRadius: 0 }} isConnectable={isConnectable} id="b" className="!bg-teal-500 !h-[10px]"/>
         </div>
-        // <div  style={{border: '2px solid purple', padding: '5px', position: 'relative', outline: outline }} className={"w-12 h-12 flex items-center justify-center rounded-full"}>
-        //     <SelectedNode selected={selected} />
-        //     <label htmlFor="file-upload"
-        //            className="inline-block w-5 h-5 rounded-full bg-white cursor-pointer">
-        //         <img src={imageLogo} alt="une icone symbolisant une image" />
-        //     </label>
-        //     <input type="file" id="file-upload" className="hidden" accept={"image/*"} onChange={onChange}/>
-        //     <Handle type="target" position={Position.Right} style={{ borderRadius: 0 }} isConnectable={isConnectable} id="b" className="!bg-teal-500 !h-[10px]"/>
-        // </div>
     );
 };
 
