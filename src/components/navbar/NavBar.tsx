f2cfab1461918567c73d94d44ba86bc51966695d
Killian <killian.wauterspro@gmail.com>
2023-07-03 10:10:24 +0200

    [suppr-connexion]

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -23,13 +23,13 @@ const NavBar = () => {
 								className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 							>
 								<span className="sr-only">View notifications</span>
-								<Link
+								{/* <Link
 									to="/login"
 									className="block rounded py-2 pr-4 pl-3 text-white"
 									aria-current="page"
 								>
 									Connexion
-								</Link>
+								</Link> */}
 							</button>
 						</div>
 					</div>
