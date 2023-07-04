a0e184b320c8f489735b416e6f04d4532a87bdf3
Paul-blgrn <paul.blgrn@gmail.com>
2023-07-04 16:27:01 +0200

    fix css questions

--- src/pages/SituationPage.tsx
+++ src/pages/SituationPage.tsx
@@ -20,7 +20,7 @@ const SituationPage = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [data]);
 
-	return <div className="box-situationpage text-white">{data && <SituationDetail situation={data} />}</div>;
+	return <div className="box-situation text-white">{data && <SituationDetail situation={data} />}</div>;
 };
 
 export default SituationPage;
