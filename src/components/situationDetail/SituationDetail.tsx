de19eadbb894ee7d322260f0adb941de268186af
Killian <killian.wauterspro@gmail.com>
2023-07-04 11:55:27 +0200

    [situationDetails]

--- src/components/situationDetail/SituationDetail.tsx
+++ src/components/situationDetail/SituationDetail.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Root } from '../../types/SituationTypes';
 
 import pls from '../../assets/images/pls-1.jpg';
+import call112 from '../../assets/images/Appel112.svg';
 
 import './SituationDetail.css';
 import AudioPlayer from '../question/AudioQuestions';
@@ -21,16 +22,19 @@ const SituationDetail = (props: Props) => {
 	};*/
 
 	return (
-		<div className="box_details">
-			<h1 className="text-center uppercase text-xl m-5 font-bold ">
+		// <div className="box_details"></div>
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				padding: 30,
+			}}
+		>
+			{/* <h1 className="text-center uppercase text-xl m-5 font-bold ">
 				{situation.situation.title}
 			</h1>
-			{/* <div className="img-box bg-gray-200 rounded-xl"></div> */}
-			{/* <img
-          src={situation.situation.image_path}
-          alt={situation.situation.title}
-          className=""
-        /> */}
+
 			<div className="box_details_img flex m-2 flex-col flex-nowrap items-center justify-between sm:flex-row">
 				<img
 					src={pls}
@@ -57,28 +61,77 @@ const SituationDetail = (props: Props) => {
 					</div>
 				</div>
 			</div>
-
-			{/* INIT Q-&-A */}
 			<div className="bg-cyan-400 hover:bg-cyan-700 rounded-full flex justify-center text-4xl m-10 button_situation">
 				<Link to={'/simulation/' + situation._id}>112</Link>
 			</div>
-			{/* RETURN TO THE 
-      LAST PAGE */}
-			{/* <div>
-        <button
-          onClick={handleGoBack}
-          className="return-button bg-gray-200 rounded-full m-4 text-2xl flex items-center gap-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 320 512"
-          >
-            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-          </svg>
-          Accueil
-        </button>
-      </div> */}
+			 */}
+			<div
+				style={{
+					display: 'flex',
+					gap: 200,
+					marginBottom: 250,
+					alignItems: 'center',
+				}}
+			>
+				<div
+					style={{
+						border: '1px solid black',
+						padding: 10,
+						textAlign: 'justify',
+						borderRadius: 10,
+						maxWidth: 650,
+						color: '#061849',
+					}}
+				>
+					<h1 style={{ paddingBottom: 10, fontWeight: 'bold', fontSize: 30 }}>
+						{situation.situation.title}
+					</h1>
+					<div
+						style={{
+							border: '1px solid black',
+							padding: 15,
+							borderRadius: 8,
+							marginBottom: 10,
+						}}
+					>
+						<p>Inconscience</p>
+					</div>
+					<h1 style={{ paddingBottom: 10, fontWeight: 'bold', fontSize: 30 }}>
+						Description de la situation
+					</h1>
+					<div
+						style={{
+							border: '1px solid black',
+							padding: 15,
+							borderRadius: 8,
+							overflow: 'scroll',
+							height: 200,
+							overflowX: 'hidden',
+						}}
+					>
+						<p>{situation.situation.description}</p>
+					</div>
+				</div>
+				<div>
+					<img
+						src={pls}
+						alt={situation.situation.title}
+						style={{
+							borderRadius: 15,
+							maxWidth: '576px',
+							height: '300px',
+							border: '2px solid red',
+						}}
+					/>
+				</div>
+			</div>
+			<div>
+				<img
+					src={call112}
+					alt={situation.situation.title}
+					style={{ borderRadius: '50%', width: 150 }}
+				/>
+			</div>
 		</div>
 	);
 };
