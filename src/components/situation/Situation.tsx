5a99cb6c243c98393d0039dd4d21dd65c523d67f
Jonathan Fenoud-Viard <jonathan.fenoud-viard@outlook.fr>
2023-07-04 11:51:37 +0200

    liste situation

--- src/components/situation/Situation.tsx
+++ src/components/situation/Situation.tsx
@@ -11,12 +11,12 @@ const Situation = (props: Props) => {
 	const { _id, situation } = props;
 
 	return (
-		<Link
+			<Link
 			to={'/situation/' + _id}
-			className="bg-gray-100 hover:bg-gray-300 shadow-lg shadow-black-500/50 tracking-wide uppercase font-bold text-center p-5 text-sm link-situation"
-		>
-			{situation.title}
-		</Link>
+			className="link-situation"
+			>
+				{situation.title}
+			</Link>
 	);
 };
 
